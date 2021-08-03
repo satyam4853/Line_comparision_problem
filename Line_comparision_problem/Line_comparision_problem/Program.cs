5ae7190e3fbb787789fc3bd63628503ea45ecf2d
Your Name <you@example.com>
2021-08-03 12:42:36 +0530

    refractor:checking equalty of two lines

--- Line_comparision_problem/Line_comparision_problem/Program.cs
+++ Line_comparision_problem/Line_comparision_problem/Program.cs
@@ -7,15 +7,28 @@ namespace Line_comparision_problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
-            int x1, x2, y1, y2;
-            Console.WriteLine("Enter values x1 and y1");
+            int x1, x2, y1, y2, x3, y3, x4, y4;
+            Console.WriteLine("Enter values for x1 x2 and y1 y2 for Line 1");
             x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values x2 and y2");
             x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("The length of the Line is : " + length);
+            Console.WriteLine("Enter values for x3 x4 and y3  y4 for Line 2");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+            double len1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double len2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            if (len1.Equals(len2))
+            {
+                Console.WriteLine(" Lines are equal in length. ");
+            }
+            else
+            {
+                Console.WriteLine(" Lines are not equal in length. ");
+            }
+
         }
     }
 }
