900694d89198eea2c5593a23272db50a9e63049c
Your Name <you@example.com>
2021-08-03 13:09:46 +0530

    refractor:comparision of two lines with end point

--- Line_comparision_problem/Line_comparision_problem/Program.cs
+++ Line_comparision_problem/Line_comparision_problem/Program.cs
@@ -8,25 +8,30 @@ namespace Line_comparision_problem
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
             int x1, x2, y1, y2, x3, y3, x4, y4;
-            Console.WriteLine("Enter values for x1 x2 and y1 y2 for Line 1");
+            Console.WriteLine("Enter values x1 x2 and y1 y2 for Line 1");
             x1 = Convert.ToInt32(Console.ReadLine());
             x2 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter values for x3 x4 and y3  y4 for Line 2");
+            Console.WriteLine("Enter values x3 x4 and y3 y4 for Line 2");
             x3 = Convert.ToInt32(Console.ReadLine());
             x4 = Convert.ToInt32(Console.ReadLine());
             y3 = Convert.ToInt32(Console.ReadLine());
             y4 = Convert.ToInt32(Console.ReadLine());
             double len1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             double len2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            if (len1.Equals(len2))
+            int diffInLength = len1.CompareTo(len2);
+            if (diffInLength == 0)
             {
-                Console.WriteLine(" Lines are equal in length. ");
+                Console.WriteLine("Lines are equal in length");
+            }
+            else if (diffInLength > 0)
+            {
+                Console.WriteLine("Line1 is greater in length than Line2");
             }
             else
             {
-                Console.WriteLine(" Lines are not equal in length. ");
+                Console.WriteLine("Line1 is smaller in length than Line2");
             }
 
         }
